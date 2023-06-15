781f3e093dcfc4d8d15f7481c1d4b8e37a04eefd
Nathalie Russo <johndoe@example.com>
2023-06-15 16:17:12 +0300

    Add Student implements

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,38 @@
-public class Main {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
+public class Main {
+//Задача 1
+//Используйте класс Student из урока 42 (см. Student.java).
+//
+//Создайте список учеников с баллами.
+//
+//Попросите пользователя ввести минимальный проходной балл.
+//
+//Выведите список учеников согласно следующим условиям:
+//
+//каждый ученик выведен в отдельной строке
+//баллы каждого ученика выше минимального проходного
+//ученики отсортированы по убыванию среднего балла
+//в списке не больше 10 учеников (извините, конкурс)
+//Для обработки и вывода результата используйте потоки (Stream).
+//
+//Примечание
+//Cортировка потока выполняется при помощи промежуточного метода sorted(Comparator<? super T> comparator).
+//
+//В качестве компаратора используйте лямбда-функцию.
+
+    public static void main(String[] args) {
+      List<Student> students = new ArrayList<>();
+      students.add(new Student("Николай", 25));
+      students.add(new Student("Виктория", 45));
+      students.add(new Student("Виктория", 5));
+      students.add(new Student("Антон", 25));
+      students.add(new Student("Борис", 15));
+
+      System.out.println(students);
+      Collections.sort(students);
+      System.out.println(students);
+    }
   }
-}
